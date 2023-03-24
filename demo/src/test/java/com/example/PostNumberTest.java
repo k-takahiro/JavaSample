151a40eea920c8c5e3490f93ba3292ae1d581384
k-takahiro <kono@nippon.co.jp>
2023-03-25 08:25:49 +0900

    PostNumberのテストプログラム追加

---
+++ demo/src/test/java/com/example/PostNumberTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.junit.Test;
+
+public class PostNumberTest {
+    @Test
+    public void testGetFormatedValue() {
+
+    }
+
+    @Test
+    public void testGetValue() {
+
+    }
+
+    @Test
+    public void testValueOf() {
+
+    }
+}
