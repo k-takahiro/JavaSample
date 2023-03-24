529f61f111628bbcb72f0a8be28bf45314fe26e4
k-takahiro <kono@nippon.co.jp>
2023-03-25 07:26:04 +0900

    PostNumber追加

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(PostNumber.valueOf("012-3456-789").getValue());
     }
 }
