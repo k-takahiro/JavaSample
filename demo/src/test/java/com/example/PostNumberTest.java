d9fdcd7c893cb52c9dd5677462203967b150a4cd
r-manome <r-manome@nippon.co.jp>
2023-03-25 09:48:02 +0900

    テストコード実装

--- demo/src/test/java/com/example/PostNumberTest.java
+++ demo/src/test/java/com/example/PostNumberTest.java
@@ -1,16 +1,25 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PostNumberTest {
+    static PostNumber target;
+
+    @BeforeClass
+    public static void init() {
+        target = new PostNumber("979-0201");
+    }
     @Test
     public void testGetFormatedValue() {
-
+        assertEquals("979-0201", target.getFormatedValue());
     }
 
     @Test
     public void testGetValue() {
-
+        assertEquals("9790201", target.getValue());
     }
 
     @Test
