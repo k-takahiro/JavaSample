15052bfea007f78ca270e3d61f2d9e66bbea5456
k-takahiro <kono@nippon.co.jp>
2023-04-03 09:14:32 +0900

    カバレッジ追加

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,14 +1,21 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println(PostNumber.valueOf("012-3456-789").getValue());
+        
+        logger.info("〇test〇");
+        logger.info(Configurator.getInstance().getValueByKey("key1"));
     }
 }
--- demo/src/test/java/com/example/PostNumberTest.java
+++ demo/src/test/java/com/example/PostNumberTest.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,11 +18,16 @@ public class PostNumberTest {
     @Test
     public void testGetFormatedValue() {
         assertEquals("979-0201", target.getFormatedValue());
+        assertTrue("979-0201".equals(target.getFormatedValue())); // 上を左記で書き換え
+        assertNotNull("getFormatedValueが失敗", target.getFormatedValue()); // getFormatedValueがNULLでないことを確認
+        // assertNotNullで引数１つのパターンを追加
     }
 
     @Test
     public void testGetValue() {
         assertEquals("9790201", target.getValue());
+        assertFalse(!"9790201".equals(target.getValue()));
+        // assertNotNull(); // getFormatedValueがNULLでないことを確認（１つと２つ）
     }
 
     @Test
