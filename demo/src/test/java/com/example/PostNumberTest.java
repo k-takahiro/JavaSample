97f071ebacc30c9ebcc9464114bfedf64028b89c
NakajunRyuma <nakajunlearn@outlook.jp>
2023-04-08 07:43:57 +0900

    テストコードを追加

--- demo/src/test/java/com/example/PostNumberTest.java
+++ demo/src/test/java/com/example/PostNumberTest.java
@@ -1,6 +1,10 @@
 package com.example;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,11 +19,19 @@ public class PostNumberTest {
     @Test
     public void testGetFormatedValue() {
         assertEquals("979-0201", target.getFormatedValue());
+        assertTrue("979-0201".equals(target.getFormatedValue())); // 検証を書き換え
+        assertNotNull(target.getFormatedValue());
+        assertNotNull("target.getFormatedValue()がnullです。", target.getFormatedValue());
+//        assertNull("target.getFormatedValue()がnullです。", target.getFormatedValue());
     }
 
     @Test
     public void testGetValue() {
         assertEquals("9790201", target.getValue());
+        assertFalse(!"9790201".equals(target.getValue()));
+        assertNotNull(target.getValue());
+        assertNotNull("target.getValue()がnullです。", target.getValue());
+        
     }
 
     @Test
