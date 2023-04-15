38102eaa610224102228d734464779213f9f147b
NakajunRyuma <nakajunlearn@outlook.jp>
2023-04-15 09:02:32 +0900

    testPublicMethod3()とtestPublicMethod4() を実装

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -3,6 +3,9 @@ package com.example;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 /**
@@ -39,14 +42,20 @@ public class AppTest
     }
 
     @Test
-    public void testPublicMethod3() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public void testPublicMethod3() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
         // 引数なしのメソッドをテスト(getMethod()でメソッドを取得)
-        
+        Class<?> clazz = Class.forName("com.example.App");
+        Method method = clazz.getMethod("publicMethod", null);
+        assertEquals("publicMethod param is none", method.invoke(new App(), null));
     }
 
     @Test
-    public void testPublicMethod4() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public void testPublicMethod4() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
         // 引数ありのメソッドをテスト(getMethod()でメソッドを取得)
+        Class<?> clazz = Class.forName("com.example.App");
+        Method method = clazz.getMethod("publicMethod", String.class);
+        String pram = "test";
+        assertEquals("publicMethod param is " + pram, method.invoke(new App(), pram));
     }
 
     @Test
