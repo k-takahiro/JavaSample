c43622830f5150d937bc7f1282616500fdd5f182
r-manome <r-manome@nippon.co.jp>
2023-04-22 10:09:41 +0900

    testStaticPrivateMethodの実装

--- demo/src/main/java/com/example/Operator.java
+++ demo/src/main/java/com/example/Operator.java
@@ -3,14 +3,14 @@ package com.example;
 import java.util.function.BiFunction;
 
 public class Operator <T extends Number> {
-    public static Operator PLUS = new Operator("+", (x, y) -> x + y);
-    public static Operator MINUS = new Operator("-", (x, y) -> x - y);
+    // public static Operator PLUS = new Operator("+", (x, y) -> x + y);
+    // public static Operator MINUS = new Operator("-", (x, y) -> x - y);
 
-    private final String symbol;
-    private final BiFunction op;
+    // private final String symbol;
+    // private final BiFunction op;
     
-    public Operator(String symbol, BiFunction<T,T,T> op) {
-        this.symbol = symbol;
-        this.op = op;
-    }
+    // public Operator(String symbol, BiFunction<T,T,T> op) {
+    //     this.symbol = symbol;
+    //     this.op = op;
+    // }
 }
--- demo/src/test/java/com/example/ReflectSampleAppTest.java
+++ demo/src/test/java/com/example/ReflectSampleAppTest.java
@@ -45,8 +45,8 @@ public class ReflectSampleAppTest
     public void testPublicMethod3() throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
         // 引数なしのメソッドをテスト(getMethod()でメソッドを取得)
         Class<?> clazz = Class.forName("com.example.ReflectSampleApp");
-        Method method = clazz.getMethod("publicMethod", null);
-        assertEquals("publicMethod param is none", method.invoke(new ReflectSampleApp(), null));
+        Method method = clazz.getMethod("publicMethod");
+        assertEquals("publicMethod param is none", method.invoke(new ReflectSampleApp()));
     }
 
     @Test
@@ -62,9 +62,9 @@ public class ReflectSampleAppTest
     public void testPrivateMethod() throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         // 引数なしのメソッドをテスト
         Class<?> clazz = Class.forName("com.example.ReflectSampleApp");
-        Method method = clazz.getDeclaredMethod("privateMethod", null);
+        Method method = clazz.getDeclaredMethod("privateMethod");
         method.setAccessible(true);
-        assertEquals("privateMethod param is none", method.invoke(new ReflectSampleApp(), null));
+        assertEquals("privateMethod param is none", method.invoke(new ReflectSampleApp()));
     }
 
     @Test
@@ -74,12 +74,36 @@ public class ReflectSampleAppTest
         Method method = clazz.getDeclaredMethod("privateMethod", String.class);
         String pram = "test";
         method.setAccessible(true);
-        assertEquals("privateMethod param is " + pram, method.invoke(new ReflectSampleApp(), pram));
-    }
+        ReflectSampleApp testTarget = new ReflectSampleApp();
+        assertEquals("privateMethod param is " + pram, method.invoke(testTarget, pram));
+
+        // インスタンスメソッドの場合
+        // ReflectSampleApp (1)testTarget = new ReflectSampleApp();
+        // String returnValue = (1)testTarget.(2)privateMethod((3)pram);
+        // リフレクションでは以下で呼び出し
+        // (2)method.invoke((1)testTarget, (3)pram);
+
+        // クラスメソッドの場合
+        // String returnValue = (1)ReflectSampleApp.(2)privateStaticMethod((3)pram);
+        // リフレクションでは以下で呼び出し（クラスメソッドの呼び出しにインスタンスは不要）
+        // (2)method.invoke((1)null, (3)pram)
+        }
 
     @Test
-    public void testStaticPrivateMethod() {
+    public void testStaticPrivateMethod() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         // privateStaticMethodの結果を確認
+        // 第一引数：メソッド名（文字列）、第二引数：メソッドの引数（型（String.class等））（なければ指定不要）
+        // 第一引数で指定したメソッド（privateStaticMethod）を取得する
+        Method method = ReflectSampleApp.class.getDeclaredMethod("privateStaticMethod");
+        // プライベートメソッドなのでアクセス許可を与える（パブリックメソッドは不要）
+        method.setAccessible(true);
+        // 第一引数：インスタンス、第二引数：メソッドの引数（実際の値（"test"等））（なければ指定不要）　
+        String returnValue = (String) method.invoke(null);
+        // 第一引数：期待値、第二引数：実際の値（テスト対象）
+        assertEquals("privateStaticMethod param is none", returnValue);
+        // privateメソッドのため呼び出せないが呼び出す場合は以下のイメージ
+        // String returnValue = ReflectSampleApp.privateStaticMethod();
+
     }
 
     @Test
